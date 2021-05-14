c669ee392debd167afacd4f99c97d495908b824c
Liam Stewart <liamchristopher.media@gmail.com>
2021-05-14 14:23:46 -0700

    finished get route for workouts/range

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,26 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout.js");
+const { db } = require("../models/Workout.js");
+
 
 
 
 // Show workouts
 router.get("/api/workouts", (req, res) => {
-    
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "exercises.duration"
+                }
+            }
+        }
+    ])
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
 
 // Creates workout
@@ -19,14 +34,29 @@ router.post("/api/workouts", ({ body }, res) => {
         });
 });
 
-// Continue workout
+// 
 router.put("/api/workouts/:id", (req, res) => {
-
+    
 });
 
 // Get all workouts in range
 router.get("/api/workouts/range", (req, res) => {
-
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration'
+                }
+            }
+        }
+    ]).limit(7)
+        .sort({_id: -1})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
 
 module.exports = router;
